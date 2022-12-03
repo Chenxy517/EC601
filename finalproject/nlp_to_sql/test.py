937f5dc74dbe073ac70f860f37a8384075ac871b
chenxy <cxy20000517@gmail.com>
2022-12-03 17:50:43 -0500

    update

--- finalproject/nlp_to_sql/test.py
+++
@@ -1,5 +0,0 @@
-from stanfordcorenlp import StanfordCoreNLP
-
-nlp = StanfordCoreNLP(r'E:\Downloads\stanford-corenlp-4.5.1')
-
-print (nlp.parse('Tell me the number of students in EC601 on Wednesday.'))
